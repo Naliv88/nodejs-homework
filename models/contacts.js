61d301373251918d539f46b52db23ec836ea54af
Naliv88 <nalivpv@gmail.com>
2023-03-19 20:31:17 +0200

    HW3 mongodb full

--- models/contacts.js
+++ models/contacts.js
@@ -1,71 +1,34 @@
-const fs = require("fs").promises;
-const uuid = require("uuid");
-const path = require("path");
-
-const contactsFilePath = path.join(__dirname, "../models/contacts.json");
-
-const readContacts = async (filePath) => {
-  try {
-    const data = await fs.readFile(filePath);
-    return JSON.parse(data);
-  } catch (error) {
-    console.log(error.message);
-    return [];
-  }
-};
-
-const writeContacts = async (contacts, filePath) => {
-  try {
-    await fs.writeFile(filePath, JSON.stringify(contacts));
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+const Contact = require("./contactModel");
 
 const listContacts = async () => {
-  const data = await readContacts(contactsFilePath);
-  return data;
+  return Contact.find();
 };
 
 const getContactById = async (contactId) => {
-  const data = await readContacts(contactsFilePath);
-  const contact = data.find(({ id }) => id === contactId);
-  return contact || null;
+  return Contact.findOne(contactId) || null;
 };
 
 const removeContact = async (contactId) => {
-  const data = await readContacts(contactsFilePath);
-  const index = data.findIndex(({ id }) => id === contactId);
-  if (index < 0) {
-    return null;
-  }
-  data.splice(index, 1);
-  await writeContacts(data, contactsFilePath);
-  return contactId;
+  return Contact.findByIdAndRemove({ _id: contactId });
 };
 
 const addContact = async (body) => {
-  const data = await readContacts(contactsFilePath);
-  const newContact = { ...body, id: uuid.v4() };
-  const newContacts = [...data, newContact];
-  await writeContacts(newContacts, contactsFilePath);
-  return newContact;
+  return Contact.create(body);
 };
 
 const updateContact = async (contactId, body) => {
-  const data = await readContacts(contactsFilePath);
-  const index = data.findIndex(({ id }) => id === contactId);
-  if (index < 0) {
-    return null;
-  }
-  const contact = data[index];
-  const updatedContact = {
-    ...contact,
-    ...body,
-  };
-  data.splice(index, 1, updatedContact);
-  await writeContacts(data, contactsFilePath);
-  return updatedContact;
+  return Contact.findByIdAndUpdate(
+    { _id: contactId },
+    { ...body },
+    { new: true }
+  );
+};
+
+const updateStatusContact = async (contactId, body) => {
+  const doc = await Contact.findById(contactId);
+  doc.favorite = body.favorite;
+  await doc.save();
+  return doc;
 };
 
 module.exports = {
@@ -74,4 +37,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
+  updateStatusContact,
 };
--- routes/middlewares/contactMiddlewares.js
+++ routes/middlewares/contactMiddlewares.js
@@ -1,53 +1,41 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const schema = Joi.when(Joi.ref("$requestMethod"), {
   switch: [
     {
       is: "POST",
       then: Joi.object({
-        name: Joi.string()
-          .min(3)
-          .max(30)
-          .required()
-          .allow(''),
+        name: Joi.string().min(3).max(30).required().allow(""),
 
-        email: Joi.string()
-          .min(6)
-          .max(30)
-          .email()
-          .required()
-          .allow(''),
+        email: Joi.string().min(6).max(30).email().required().allow(""),
 
         phone: Joi.string()
           .length(13)
           .pattern(/^\+380\d{9}$/)
           .required()
-          .allow(''),
+          .allow(""),
+
+        favorite: Joi.boolean(),
       }),
     },
     {
       is: "PUT",
       then: Joi.object({
-        name: Joi.string()
-          .min(3)
-          .max(30)
-          .allow(''),
+        name: Joi.string().min(3).max(30).allow(""),
 
-        email: Joi.string()
-          .min(6)
-          .max(30)
-          .email()
-          .allow(''),
+        email: Joi.string().min(6).max(30).email().allow(""),
 
         phone: Joi.string()
           .length(13)
           .pattern(/^\+380\d{9}$/)
-          .allow(''),
+          .allow(""),
+
+        favorite: Joi.boolean(),
       }),
     },
   ],
 });
 
 module.exports = {
-  schema
-}
+  schema,
+};
